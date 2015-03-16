4730d12627321736ffad9e006c0a0a30a837d608
Wasiak <qbawasiak@wp.pl>
2015-03-16 17:52:47 +0100

    sorting all categories

--- main.js
+++ main.js
@@ -128,7 +128,10 @@ var rebuildTable = function() {
 	for (var book in collection) {
 		mainTable.appendChild(collection[book].element);
 	}
-	getPageSorter();			// should  get new values for row,cols... every time table is rebuilded
+	getPageSorter();
+	getYearSorter();
+	getTitleSorter();
+	getPlTitleSorter();
 };
 var pagesDesc = false; //malejaco = fałsz
 // When button is clicked
@@ -138,14 +141,11 @@ var cols;
 var pageCol;
 
 var getPageSorter = function(){
-	console.log('odpalam funkcje getPageSorter');
-row = document.getElementsByTagName('tr');		//every row
-categoryRow = row[0];		// first row (category)
+console.log('odpalam funkcje getPageSorter');
+row = document.getElementsByTagName('tr');			//every row
+categoryRow = row[0];								// first row (category)
 cols = categoryRow.getElementsByTagName('td')		//every td in first row
-pageCol = cols[4];			// 5th column in category row (paGE COL)
-}
-																						// work only once ;/ ??!
-getPageSorter();
+pageCol = cols[4];									// 5th column in category row (paGE COL)
 pageCol.addEventListener('click', function(){
 	// sport the collection array
 	collection = collection.sort(sortByPageFilter);
@@ -155,18 +155,23 @@ pageCol.addEventListener('click', function(){
 	// and rebuild table
 	rebuildTable();
 	pagesDesc = !pagesDesc;
-	//getPageSorter();
 });
+}
+getPageSorter();
 
 searchBar.onkeyup = search;
-
 // sorting
-
 var sortByYearFilter = function(a, b) {
 	return a.year - b.year;
 } 
 var yearDesc = false;
-document.getElementById('sortByYear').addEventListener('click', function(){
+var yearCol;
+var getYearSorter = function(){
+	row = document.getElementsByTagName('tr');			//every row
+	categoryRow = row[0];								// first row (category)
+	cols = categoryRow.getElementsByTagName('td')		//every td in first row
+	yearCol = cols[3];									// 4th column in  category row
+	yearCol.addEventListener('click', function(){
 		collection = collection.sort(sortByYearFilter);
 	if (yearDesc){
 		collection = collection.reverse();
@@ -174,7 +179,8 @@ document.getElementById('sortByYear').addEventListener('click', function(){
 	rebuildTable();
 	yearDesc = !yearDesc;
 });
-
+}
+getYearSorter();
 var sortByTitleFilter = function(a, b){
 	var a = a.title.toLowerCase();
 	var b = b.title.toLowerCase();
@@ -182,7 +188,13 @@ var sortByTitleFilter = function(a, b){
 	if (a > b){ return 1}	
 }
 var titleDesc = false;
-document.getElementById('sortByTitle').addEventListener('click', function(){
+var titleCol;
+var getTitleSorter = function(){
+	row = document.getElementsByTagName('tr');			//every row
+	categoryRow = row[0];								// first row (category)
+	cols = categoryRow.getElementsByTagName('td')		//every td in first row
+	titleCol = cols[1];									// second column in category row	
+	titleCol.addEventListener('click', function(){
 	collection = collection.sort(sortByTitleFilter);
 	if (titleDesc){
 		collection = collection.reverse();
@@ -190,7 +202,8 @@ document.getElementById('sortByTitle').addEventListener('click', function(){
 	rebuildTable();
 	titleDesc = !titleDesc;			
 });
-
+}
+getTitleSorter();
 var sortByPlTitleFilter = function(a, b){
 	var a = a.pl.toLowerCase();
 	var b = b.pl.toLowerCase();
@@ -198,7 +211,13 @@ var sortByPlTitleFilter = function(a, b){
 	if (a > b){ return 1}	
 }
 var plTitleDesc = false;
-document.getElementById('sortByPlTitle').addEventListener('click', function(){
+var plTitleCol;
+var getPlTitleSorter = function(){
+	row = document.getElementsByTagName('tr');			//every row
+	categoryRow = row[0];								// first row (category)
+	cols = categoryRow.getElementsByTagName('td')		//every td in first row
+	plTitleCol = cols[2];									// third column in category row	
+	plTitleCol.addEventListener('click', function(){
 	collection = collection.sort(sortByPlTitleFilter);
 	if (plTitleDesc){
 		collection = collection.reverse();
@@ -206,3 +225,5 @@ document.getElementById('sortByPlTitle').addEventListener('click', function(){
 	rebuildTable();
 	plTitleDesc = !plTitleDesc;			
 });
+}
+getPlTitleSorter();
